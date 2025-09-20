c675e92fec929a38ada7558bffa3d8f8f1b97ffd
CristianSbarboza <cristianbsconcursos@gmail.com>
2025-09-20 12:20:58 -0300

    add url db render

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n0_ban9#vu4s)0uyv_g2px6i$l$%ytv97(ofwhdfqzjwun56l4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,15 +75,24 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'learning_db',
+#         'USER': 'cristian',
+#         'PASSWORD': '@tsukBS2',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'learning_db',
-        'USER': 'cristian',
-        'PASSWORD': '@tsukBS2',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')  # Render vai fornecer DATABASE_URL automaticamente
+    )
 }
 
 
@@ -121,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / "staticfiles"  # onde o collectstatic vai colocar os arquivos
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
