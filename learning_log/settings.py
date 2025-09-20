e30bae8b7b031611a78fbc4f87e732547732a588
CristianSbarboza <cristianbsconcursos@gmail.com>
2025-09-20 12:46:30 -0300

    /static

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_ROOT = BASE_DIR / "staticfiles"  # onde o collectstatic vai colocar os arquivos
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # se tiver static global
